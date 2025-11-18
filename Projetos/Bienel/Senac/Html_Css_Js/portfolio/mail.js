bd6620b08ae69fbfdfe24d430671476367f72712
Leandro Bernardino Vieira <leandrovieira3243@gmail.com>
2025-11-17 21:35:50 -0300

    update

--- Projetos/Bienel/Senac/Html_Css_Js/portfolio/mail.js
+++ Projetos/Bienel/Senac/Html_Css_Js/portfolio/mail.js
@@ -1,6 +1,9 @@
 // Função Enviar E-mail //
 
-var nome = ( '#name' );
-var email = ( '#email' );
-var topico = ( '#subject' );
-var mensagem = ( '#content' );
+var send = document.getElementById( 'send' );
+
+var name = document.getElementById( 'name' ).value;
+var email = document.getElementById( 'email' ).value;
+var subject = document.getElementById( 'subject' ).value;
+var content = document.getElementById( 'content' ).value;
+var body = 'name: ' + name + '<br> e-mail: ' + email + ' <br> subject: ' + subject + ' <br> mensagem: ' + content;
