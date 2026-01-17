a4913c9fe7477183ccb0ddcedcfd65736608b993
Leandro <leandrovieira3243@gmail.com>
2026-01-16 22:47:02 -0300

    Create t1.js

---
+++ t&stes/t1.js
@@ -0,0 +1,15 @@
+// voltei depois de tanto tempo jesus kskks
+// agr irei testar meu conhecimento pra ver se ainda tô bom
+/*
+    let velocidade = 81;
+    const velmax = 80;
+    if (velocidade >= velmax) {
+        console.log('Tá muito rápido seu burro, desacelera ou eu te desacelero na porrada!');
+    } else {
+        console.log('tudo certo até aqui, vai seguindo certo!')
+    }
+*/
+
+for (let cal = 0;cal < 2; cal++) {
+    console.log('boiola demais!');
+}
